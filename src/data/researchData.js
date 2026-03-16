6cc30f094aab06e6173d375e7b4afa43a016f9d2
Lakshya7199 <lakshya.datir2001@gmail.com>
2026-03-16 13:05:39 +0530

    Updating the portfolio

--- src/data/researchData.js
+++ src/data/researchData.js
@@ -15,6 +15,12 @@ export const researchData = {
       summary:
         "Automate repetitive website tests using AWS CloudWatch Synthetic Canaries, which mimic real user behavior on a schedule, detect failures early, capture screenshots, and alert you—saving time and avoiding manual testing.",
       link: "https://medium.com/@lakshya.datir2001/stop-manually-testing-your-website-automate-it-with-aws-synthetic-canaries-6b18b8e26dd5?postPublishedType=initial",
+    },
+    {
+      title: "How I Cut AWS Data Processing Costs by 70% Using a Smarter Multi-Account Architecture",
+      summary:
+        "Exploring a more efficient multi-account strategy to reduce AWS data processing costs while maintaining security and compliance.",
+      link: "https://medium.com/@lakshya.datir2001/how-i-cut-aws-data-processing-costs-by-70-using-a-smarter-multi-account-architecture-57b9e8070eee",
     }
   ],
 };
